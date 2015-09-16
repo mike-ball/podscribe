9b982d70d9d4b47a7b97d942535c360b7ebdc221
Mike Ball <mike@snoogen.com>
2015-09-15 22:43:49 -0600

    ignoring secrets.yml and symlinking it with capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -47,6 +47,7 @@ namespace :deploy do
 
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/secrets.yml #{release_path}/config/secrets.yml"
     # run "ln -nfs #{shared_path}/exports #{release_path}/exports"
   end
   after "deploy:finalize_update", "deploy:symlink_config"
